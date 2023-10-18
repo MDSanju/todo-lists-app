628a76d42b1ee4d2affa1481b19f99e86691de4f
MDSanju <mdsanju387@gmail.com>
2023-10-18 11:08:49 +0600

    Clipboard copy feature & all bugs fix [DONE]

--- src/Pages/Categories/Categories.tsx
+++ src/Pages/Categories/Categories.tsx
@@ -1,7 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useForm, isNotEmpty } from "@mantine/form";
-import { Button, Group, TextInput, Box } from "@mantine/core";
+import {
+  Button,
+  Group,
+  TextInput,
+  Box,
+  Title,
+  Tabs,
+  Grid,
+  rem,
+} from "@mantine/core";
+import { IconCategory, IconHome } from "@tabler/icons-react";
 
 import { Category } from "../../types";
 import { ListContext } from "../../context";
@@ -10,6 +21,10 @@ import { capitalizeFirstLetter } from "../../Shared/utils";
 const Categories = () => {
   const { createCategory } = useContext(ListContext);
   const [categories, setCategories] = useState<Category[]>([]);
+
+  const navigate = useNavigate();
+
+  const iconStyle = { width: rem(16), height: rem(16) };
 
   const createCategoryForm = useForm({
     initialValues: {
@@ -30,6 +45,14 @@ const Categories = () => {
     createCategoryForm.reset();
   };
 
+  const landHome = () => {
+    navigate("/home");
+  };
+
+  const landCategories = () => {
+    navigate("/categories");
+  };
+
   const allCategoriesArray: Category[] = categories.map((category: any) => ({
     category: capitalizeFirstLetter(category?.category),
   }));
@@ -47,39 +70,75 @@ const Categories = () => {
   }, []);
 
   return (
-    <Box>
-      <Box
-        component="form"
-        maw={400}
-        mx="auto"
-        onSubmit={createCategoryForm.onSubmit((values) =>
-          handleCreateCategory(values)
-        )}
+    <Box style={{ margin: "24px 42px 64px 42px" }}>
+      <Title
+        order={2}
+        mb={24}
+        style={{ display: "flex", justifyContent: "center" }}
       >
-        <TextInput
-          label="Category Name"
-          placeholder="Category name"
-          withAsterisk
-          {...createCategoryForm.getInputProps("category")}
-        />
+        TODO List Items
+      </Title>
 
-        <Group justify="flex-end" mt="md">
-          <Button type="submit">Submit</Button>
-        </Group>
+      <Box>
+        <Tabs defaultValue="categories">
+          <Tabs.List>
+            <Tabs.Tab
+              value="gallery"
+              onClick={landHome}
+              leftSection={<IconHome stroke={1.5} style={iconStyle} />}
+            >
+              Home
+            </Tabs.Tab>
+            <Tabs.Tab
+              value="categories"
+              onClick={landCategories}
+              leftSection={<IconCategory stroke={1.5} style={iconStyle} />}
+            >
+              Categories
+            </Tabs.Tab>
+          </Tabs.List>
+        </Tabs>
       </Box>
 
-      <Box mt="md" mx="auto">
-        <h2>All Categories:</h2>
-        {primitiveCategories.length > 0 ? (
-          <ul>
-            {primitiveCategories.map((category, index) => (
-              <li key={index}>{category.category}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No data found!</p>
-        )}
-      </Box>
+      <Grid gutter="xl">
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <Box
+            component="form"
+            mt={48}
+            style={{ width: "100%" }}
+            onSubmit={createCategoryForm.onSubmit((values) =>
+              handleCreateCategory(values)
+            )}
+          >
+            <TextInput
+              label="Category Name"
+              placeholder="Category name"
+              variant="filled"
+              withAsterisk
+              {...createCategoryForm.getInputProps("category")}
+            />
+
+            <Group justify="flex-end" mt="md">
+              <Button type="submit">Submit</Button>
+            </Group>
+          </Box>
+        </Grid.Col>
+
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <Box mt={48} style={{ width: "100%" }}>
+            <h2>All Categories:</h2>
+            {primitiveCategories.length > 0 ? (
+              <ul>
+                {primitiveCategories.map((category, index) => (
+                  <li key={index}>{category.category}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>No data found!</p>
+            )}
+          </Box>
+        </Grid.Col>
+      </Grid>
     </Box>
   );
 };
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useForm, isNotEmpty } from "@mantine/form";
 import {
   Button,
@@ -8,7 +9,24 @@ import {
   Box,
   Select,
   CloseButton,
+  Grid,
+  Title,
+  Tabs,
+  rem,
+  Card,
+  Text,
+  Alert,
+  Tooltip,
+  Pagination,
 } from "@mantine/core";
+import {
+  IconCategory,
+  IconCopy,
+  IconDatabaseOff,
+  IconHome,
+  IconListDetails,
+  IconSearch,
+} from "@tabler/icons-react";
 
 import { ListContext } from "../../context";
 import { Category, Item, SelectInput } from "../../types";
@@ -20,6 +38,23 @@ const Home = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [findItem, setFindItem] = useState<string>("");
+  const [copied, setCopied] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemsPerPage = 5;
+
+  const navigate = useNavigate();
+
+  const iconStyle = { width: rem(16), height: rem(16) };
+
+  const allCategoriesArray: SelectInput[] = categories.map((category: any) => ({
+    value: capitalizeFirstLetter(category?.category),
+    label: category?.category,
+  }));
+
+  const selectCategory = [
+    ...new Set(allCategoriesArray.map((category) => category.value)),
+  ].map((value) => ({ value, label: value }));
 
   const createItemForm = useForm({
     initialValues: {
@@ -29,7 +64,11 @@ const Home = () => {
 
     validate: {
       itemName: isNotEmpty("Item name is required!"),
-      itemCategory: isNotEmpty("Select an item category!"),
+      itemCategory: isNotEmpty(
+        selectCategory.length
+          ? "Select an item category!"
+          : "Please create categories!"
+      ),
     },
   });
 
@@ -42,27 +81,38 @@ const Home = () => {
     createItemForm.reset();
   };
 
-  const allCategoriesArray: SelectInput[] = categories.map((category: any) => ({
-    value: capitalizeFirstLetter(category?.category),
-    label: category?.category,
-  }));
+  const uniqueItemIdentifiers = new Set();
 
-  const selectCategory = [
-    ...new Set(allCategoriesArray.map((category) => category.value)),
-  ].map((value) => ({ value, label: value }));
+  const uniqueItems = items.filter((item) => {
+    const itemIdentifier = `${item.itemName.toLowerCase()}-${item.itemCategory.toLowerCase()}`;
+    if (uniqueItemIdentifiers.has(itemIdentifier)) {
+      return false;
+    }
+
+    uniqueItemIdentifiers.add(itemIdentifier);
+    return true;
+  });
+
+  const handlePageChange = (newPage: any) => {
+    setCurrentPage(newPage);
+  };
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = Math.min(startIndex + itemsPerPage, uniqueItems.length);
+  const itemsToShow = uniqueItems.slice(startIndex, endIndex);
 
   let filteredItems;
 
   if (searchQuery.length > 0) {
-    filteredItems = items.filter((item) =>
+    filteredItems = uniqueItems.filter((item) =>
       item.itemName.toLowerCase().includes(searchQuery?.toLowerCase())
     );
   } else if (findItem.length > 0) {
-    filteredItems = items.filter((item) =>
+    filteredItems = uniqueItems.filter((item) =>
       item.itemCategory.toLowerCase().includes(findItem?.toLowerCase())
     );
   } else {
-    filteredItems = items;
+    filteredItems = itemsToShow;
   }
 
   const itemCategoriesArray: SelectInput[] = items.map((item: any) => ({
@@ -82,6 +132,27 @@ const Home = () => {
     }
   };
 
+  const landHome = () => {
+    navigate("/home");
+  };
+
+  const landCategories = () => {
+    navigate("/categories");
+  };
+
+  const copyToClipboard = async (text: any) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setCopied(true);
+
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    } catch (error) {
+      console.error("Failed to copy to clipboard:", error);
+    }
+  };
+
   useEffect(() => {
     const storedCategories = JSON.parse(
       localStorage.getItem("categoryList") || "[]"
@@ -97,82 +168,199 @@ const Home = () => {
   }, []);
 
   return (
-    <Box>
-      <Box
-        component="form"
-        maw={400}
-        mx="auto"
-        onSubmit={createItemForm.onSubmit((values) => handleCreateItem(values))}
+    <Box style={{ margin: "24px 42px 64px 42px" }}>
+      <Title
+        order={2}
+        mb={24}
+        style={{ display: "flex", justifyContent: "center" }}
       >
-        <TextInput
-          label="Item Name"
-          placeholder="Item name"
-          withAsterisk
-          {...createItemForm.getInputProps("itemName")}
-        />
+        TODO List Items
+      </Title>
 
-        <Select
-          label="Item Categories"
-          placeholder="Select category"
-          data={selectCategory}
-          withScrollArea={false}
-          styles={{ dropdown: { maxHeight: 200, overflowY: "auto" } }}
-          mt="md"
-          withAsterisk
-          clearable
-          {...createItemForm.getInputProps("itemCategory")}
-        />
-
-        <Group justify="flex-end" mt="md">
-          <Button type="submit">Submit</Button>
-        </Group>
+      <Box>
+        <Tabs defaultValue="gallery">
+          <Tabs.List>
+            <Tabs.Tab
+              value="gallery"
+              onClick={landHome}
+              leftSection={<IconHome stroke={1.5} style={iconStyle} />}
+            >
+              Home
+            </Tabs.Tab>
+            <Tabs.Tab
+              value="categories"
+              onClick={landCategories}
+              leftSection={<IconCategory stroke={1.5} style={iconStyle} />}
+            >
+              Categories
+            </Tabs.Tab>
+          </Tabs.List>
+        </Tabs>
       </Box>
 
-      <Box mx="auto" mt="xl" style={{ width: "400px" }}>
-        <TextInput
-          placeholder="Search items"
-          value={searchQuery}
-          onChange={(event) => setSearchQuery(event.currentTarget.value)}
-          rightSection={
-            <CloseButton
-              c="gray"
-              size={24}
-              aria-label="Clear input"
-              onClick={() => setSearchQuery("")}
-              style={{ display: searchQuery ? undefined : "none" }}
-            />
-          }
-        />
-      </Box>
+      <Grid gutter="xl">
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <Text fw={500} mt={32} mb={8}>
+            Add A New Item
+          </Text>
+          <Card shadow="sm" withBorder>
+            <Box
+              component="form"
+              style={{ width: "100%", padding: "8px 8px" }}
+              onSubmit={createItemForm.onSubmit((values) =>
+                handleCreateItem(values)
+              )}
+            >
+              <Text size="sm" c="gray" mb={16}>
+                User can add a new item to the list by an item mane and
+                selecting an item category.
+              </Text>
+              <TextInput
+                label="Item Name"
+                placeholder="Item name"
+                variant="filled"
+                withAsterisk
+                {...createItemForm.getInputProps("itemName")}
+              />
 
-      <Box mx="auto" mt="xl" style={{ width: "400px" }}>
-        <Select
-          label="Item Categories"
-          placeholder="Select category"
-          data={selectData}
-          withScrollArea={false}
-          styles={{ dropdown: { maxHeight: 200, overflowY: "auto" } }}
-          mt="md"
-          withAsterisk
-          clearable
-          onChange={findByCategory}
-        />
-      </Box>
+              <Select
+                label="Item Categories"
+                placeholder="Select category"
+                variant="filled"
+                data={selectCategory}
+                withScrollArea={false}
+                styles={{ dropdown: { maxHeight: 200, overflowY: "auto" } }}
+                mt="md"
+                withAsterisk
+                clearable
+                {...createItemForm.getInputProps("itemCategory")}
+              />
 
-      <Box mt="md" mx="auto">
-        <h2>All Items:</h2>
-        {filteredItems.length > 0 ? (
-          <ul>
-            {filteredItems.map((item, index) => (
-              <li key={index}>
-                {item.itemName} - {item.itemCategory}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No data found!</p>
-        )}
-      </Box>
+              <Group justify="flex-end" mt="md">
+                <Button type="submit">Add Item</Button>
+              </Group>
+            </Box>
+          </Card>
+        </Grid.Col>
+
+        <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
+          <Text fw={500} mt={32} mb={8}>
+            List of Items
+          </Text>
+          <Card shadow="sm" withBorder mb={48}>
+            <Box style={{ width: "100%", padding: "8px 8px" }}>
+              <Text size="sm" c="gray" mb={16}>
+                User can search items by item name & filter items by selecting
+                category and view items list.
+              </Text>
+              <Grid>
+                <Grid.Col span={{ base: 12, md: 8, lg: 8 }}>
+                  <Box>
+                    <TextInput
+                      label="Search by Item Name"
+                      placeholder="Search item"
+                      variant="filled"
+                      leftSection={<IconSearch size={16} stroke={1.5} />}
+                      value={searchQuery}
+                      onChange={(event) =>
+                        setSearchQuery(event.currentTarget.value)
+                      }
+                      rightSection={
+                        <CloseButton
+                          c="gray"
+                          size={24}
+                          aria-label="Clear input"
+                          onClick={() => setSearchQuery("")}
+                          style={{ display: searchQuery ? undefined : "none" }}
+                        />
+                      }
+                    />
+                  </Box>
+                </Grid.Col>
+                <Grid.Col span={{ base: 12, md: 4, lg: 4 }}>
+                  <Box>
+                    <Select
+                      label="Filter Items by Category"
+                      placeholder="Select category"
+                      variant="filled"
+                      data={selectData}
+                      withScrollArea={false}
+                      styles={{
+                        dropdown: { maxHeight: 200, overflowY: "auto" },
+                      }}
+                      clearable
+                      onChange={findByCategory}
+                    />
+                  </Box>
+                </Grid.Col>
+              </Grid>
+
+              <Box mt="md">
+                <Text fw={700} mt={24} mb={8} size="xl">
+                  List Items:
+                </Text>
+                {filteredItems.length > 0 ? (
+                  <>
+                    {filteredItems.map((item, index) => (
+                      <Box key={index} mb={16}>
+                        <Group justify="space-between" grow gap={0}>
+                          <Alert
+                            variant="light"
+                            color="rgba(27, 117, 135, 0.65)"
+                            title={item.itemName}
+                            icon={<IconListDetails size={20} stroke={1.5} />}
+                            style={{ width: "86%" }}
+                          >
+                            Category: {item.itemCategory}
+                          </Alert>
+
+                          <Box style={{ width: "8%", paddingLeft: "8px" }}>
+                            <Tooltip
+                              label={copied ? "Copied!" : "Copy Clipboard!"}
+                              withArrow
+                            >
+                              <IconCopy
+                                size={25}
+                                stroke={1.5}
+                                color="gray"
+                                cursor="pointer"
+                                onClick={() => copyToClipboard(item.itemName)}
+                              />
+                            </Tooltip>
+                          </Box>
+                        </Group>
+                      </Box>
+                    ))}
+                  </>
+                ) : (
+                  <Box
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: "24px",
+                    }}
+                  >
+                    <IconDatabaseOff color="gray" size={24} stroke={1.5} />
+                    <Text fz="xs" color="gray">
+                      No records
+                    </Text>
+                  </Box>
+                )}
+              </Box>
+            </Box>
+            {searchQuery.length === 0 && findItem.length === 0 && (
+              <Pagination
+                total={Math.ceil(uniqueItems.length / itemsPerPage)}
+                value={currentPage}
+                onChange={handlePageChange}
+                mb={16}
+              />
+            )}
+          </Card>
+        </Grid.Col>
+      </Grid>
     </Box>
   );
 };
